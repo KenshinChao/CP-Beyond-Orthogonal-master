5fc5e1da7958a5c965760cdaeeb9eda6c244144a
Kenshin Chao <kechchao@ucsc.edu>
2024-01-26 15:48:25 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// Code Practice: Beyond Orthogonal
+// Name: Kenshin Chao
+// Date: 01/26/2024
+
+// Spritesheet by ElvGames: https://elv-games.itch.io/free-fantasy-dreamland-sprites
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 800,
+    render: {
+      pixelArt: true  
+    },
+    physics: {
+        default: 'arcade',
+        debug: true
+    },
+    scene: [ Movement ]
+    
+}
+
+let game = new Phaser.Game(config)
+
+let cursors 
+let { height, width } = game.config
